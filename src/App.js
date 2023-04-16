b2eeebe88be8e6feb111055bfec1588e39fce364
Aca <aleksandars098@gmail.com>
2023-04-16 03:55:20 +0200

    Changet code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! Howe ale you?
+          I was changed in feature branch
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactaa
+          Learn React
         </a>
       </header>
     </div>
